30e82c3abbaa9ff3a792a3b8a2239e78b56f5685
Jhonattas Ferreira <jfs4@cin.ufpe.br>
2016-05-22 02:02:35 +0000

    add login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-        flash[:success] = "Bem vindo ao Zilla"
+          log_in @user
+          flash[:success] = "Bem vindo ao Zilla"
           redirect_to @user
         else
           render 'new'
