f4207453d4979717ec26e55c9aa0fdb129c94619
Jhonattas Ferreira <jfs4@cin.ufpe.br>
2016-06-28 23:35:35 +0000

    associar ticket ao user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509193643) do
+ActiveRecord::Schema.define(version: 20160628233053) do
+
+  create_table "tickets", force: :cascade do |t|
+    t.string   "tipo"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
